920477a1ea34c8652bb7a5a601eaabde63476f7a
Stu Halliburton <stu.halliburton@gmail.com>
2014-09-23 21:42:39 -0600

    Refactor: extract EXCLUDED_WORDS

--- lib/ml_wiki.rb
+++ lib/ml_wiki.rb
@@ -1,5 +1,6 @@
 require 'ml_wiki/version'
 require 'ml_wiki/words'
+require 'ml_wiki/excluded_words'
 require 'open-uri'
 require 'nokogiri'
 # require 'redis'
@@ -21,9 +22,6 @@ module Wiki
 
     return Words.new(word_frequency, name: combined_name.join('_'))
   end
-
-
-  EXCLUDED_WORDS = %w(the of to in and was that for on as with by had at an it from has is be have which who not said been would also but this before up were then its into when however did while they than them are their or there you where so whether we even went any don't want should could would me do if more all after only like many most can other over under some such these no yes same just use using th very about he she his hers him her my what yet another)
 
   def self.classify(url, name: nil)
     doc = Nokogiri::HTML(open(url))
---
+++ lib/ml_wiki/excluded_words.rb
@@ -0,0 +1,3 @@
+module Wiki
+  EXCLUDED_WORDS = %w(the of to in and was that for on as with by had at an it from has is be have which who not said been would also but this before up were then its into when however did while they than them are their or there you where so whether we even went any don't want should could would me do if more all after only like many most can other over under some such these no yes same just use using th very about he she his hers him her my what yet another)
+end
