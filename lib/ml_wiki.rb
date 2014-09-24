de39b3bde64587c181640ebe574d6ab45b3f5704
Stu Halliburton <stu.halliburton@gmail.com>
2014-09-23 21:54:21 -0600

    Refactor: extract HCluster module

--- lib/ml_wiki.rb
+++ lib/ml_wiki.rb
@@ -2,6 +2,7 @@ require 'ml_wiki/version'
 require 'ml_wiki/words'
 require 'ml_wiki/excluded_words'
 require 'ml_wiki/pearsons_corelation'
+require 'ml_wiki/hcluster/hcluster'
 require 'open-uri'
 require 'nokogiri'
 # require 'redis'
@@ -69,45 +70,6 @@ module Wiki
   end
 
 
-  module HCluster
-    def self.cluster(collection)
-      cluster = []
-      collection.each do |item|
-        cluster << BiCluster.new(item)
-      end
-
-      while cluster.length > 1
-
-        closeness = []
-        cluster.each do |item|
-          puts item.vec.name
-          others = cluster - [item]
-          sim = Wiki.most_like(item, *others)
-          puts sim.inspect
-          closeness << [item.name, sim.first]
-        end
-
-        closeness.sort_by!{ |name, score| score.last }
-
-        puts ''
-        puts '#################################'
-        puts '### REDUCED CLUSTER CLOSENESS ###'
-        puts closeness.inspect
-        puts '#################################'
-        puts ''
-
-
-        distance = closeness[0..1].inject(0){ |total, val| total += val.last.last}/2
-        left = cluster.detect{ |c| c.name == closeness[0].first }
-        right = cluster.detect{ |c| c.name == closeness[1].first }
-        combined = Wiki.combine(left, right)
-
-        cluster << BiCluster.new(combined, left: cluster.delete(left), right: cluster.delete(right), distance: distance)
-      end
-
-      cluster.first
-    end
-  end
 end
 
 blair = Wiki.classify('http://en.wikipedia.org/wiki/Tony_Blair', name: 'blair')
